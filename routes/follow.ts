bf4d1555baecd6d1f953833b9a4cd7cfed8a18a1
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-12-25 21:24:35 +0000

    implemented /follow API route to follow and unfollow

---
+++ routes/follow.ts
@@ -0,0 +1,39 @@
+import User from "../models/User";
+import express from "express";
+import authenticate from "../middleware/authenticate";
+
+export const run = (app): void => {
+
+  app.get("/api/follow", authenticate(["social"]), 
+    async (req: express.Request, res: express.Response) => {
+    try {
+      const userA = await User.findOne({ username: req.username })
+      const userB = await User.findOne({ username: req.query.username })
+
+      if (!userA || !userB) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      // A unfollows B
+      if(userB.followers.includes(req.username)) {
+        userB.followers = userB.followers.filter(f => f !== req.username);
+        userA.following = userA.following.filter(f => f !== req.query.username);
+      } else { // A follows B
+        userB.followers.push(req.username);
+        userA.following.push(req.query.username);
+      }
+
+      await userA.save();
+      await userB.save();
+
+      res.status(200).json({ message: "User successfully updated" })
+
+    } catch (err) {
+
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+
+    }
+  });
+
+};
