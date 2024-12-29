98c3a03e3ad4b3911bee2f6d5175c6f8bf97354c
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-12-29 11:53:58 +0000

    wrote OpenAPI specification and added redoc renderer

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import fs from "fs";
 import synapse from './functions/synapse';
 import mongoose from "mongoose";
+import path from "path";
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -15,6 +16,7 @@ const app = synapse("synapse-auth", process.env.SYNAPSE_KEY);
 app.use(cors());
 app.use(express.json())
 
+app.use("/assets", express.static("assets"));
 app.use("/auth", express.static("auth"));
 app.use("/dist", express.static("dist"));
 
@@ -24,6 +26,25 @@ for(let endpoint of fs.readdirSync('./routes')) {
   let { run } = await import(`./routes/${endpoint}`);
   run(app, CODES);
 }
+
+app.get("/openapi", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "openapi.yaml"));
+});
+
+app.get("/", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Synapse API Docs</title>
+    </head>
+    <body>
+      <redoc spec-url="/openapi"></redoc>
+      <script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"></script>
+    </body>
+    </html>
+  `);
+});
 
 app.listen(8080, () => {
   console.log(`Server listening to port ${8080}`);
