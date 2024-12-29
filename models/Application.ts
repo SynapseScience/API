71ace707d21b750fea3ce585aa47058a8b70e101
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-12-29 11:52:37 +0000

    added detailed fields in both User and Application models

--- models/Application.ts
+++ models/Application.ts
@@ -2,12 +2,31 @@ import mongoose, { Schema, Document } from "mongoose";
 
 const applicationSchema = new Schema({
   client_id: { type: String, required: true, unique: true },
-  permissions: [{ type: String, required: true }]
+  title: { type: String, required: true },
+  creation: { type: Date, default: Date.now },
+  permissions: [{ type: String }],
+  stargazers: [{ type: String }],
+  description: { type: String, default: "This app has no description" },
+  link: {
+    type: String,
+    required: false,
+    validate: {
+      validator: function (v: string) {
+        return /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid URL!`,
+    },
+  }
 });
 
 interface IApplication extends Document {
   client_id: string;
+  title: string;
+  creation: Date;
   permissions: string[];
+  stargazers: string[];
+  description: string;
+  link: string;
 }
 
 const Application = mongoose.model<IApplication>("Client", applicationSchema);
--- models/User.ts
+++ models/User.ts
@@ -6,7 +6,7 @@ const userSchema = new Schema({
   fullname: { type: String, required: false },
   description: { type: String, default: "This user has no description" },
   email: { type: String, required: true, unique: true },
-  pronouns: { type: String, required: false },
+  pronouns: { type: String, enum: ["il", "elle", "iel"], required: true },
   avatar: { type: String, required: false },
   account: { type: Number, default: 100 },
   following: [{ type: String }],
@@ -16,26 +16,26 @@ const userSchema = new Schema({
     orcid: { type: String, required: false },
     bluesky: { type: String, required: false },
     github: { type: String, required: false },
+    gitlab: { type: String, required: false },
     hal: { type: String, required: false },
-    huggingface: { type: String, required: false }
-  }
+    huggingface: { type: String, required: false },
+    x: { type: String, required: false },
+    instagram: { type: String, required: false },
+    linkedin: { type: String, required: false },
+    reddit: { type: String, required: false },
+    discord: { type: String, required: false },
+    wechat: { type: String, required: false },
+    threads: { type: String, required: false },
+    mastodon: { type: String, required: false },
+    goodreads: { type: String, required: false },
+  },
+  creation: { type: Date, default: Date.now },
+  starred: [{ type: String }]
 });
 
-userSchema.statics.getPublicFields = function () {
-  const publicFields = [
-    "username",
-    "fullname",
-    "description",
-    "pronouns",
-    "avatar",
-    "following",
-    "followers",
-    "badges",
-    "connections"
-  ];
-
-  return publicFields.join(' ');
-}
+userSchema.methods.publicFields = function() {
+  return this.model('User').findOne({ _id: this._id }).select('-password -account -email');
+};
 
 interface IUser extends Document {
   username: string;
@@ -43,17 +43,28 @@ interface IUser extends Document {
   fullname?: string;
   avatar?: string;
   description: string;
-  pronouns: string;
+  pronouns: "il" | "elle" | "iel";
   account: number;
   following: string[];
   followers: string[];
   badges: string[];
+  creation: Date;
+  starred: string[];
   connections: {
     orcid?: string;
     bluesky?: string;
     github?: string;
     hal?: string;
     huggingface?: string;
+    x?: string;
+    instagram?: string;
+    linkedin?: string;
+    reddit?: string;
+    discord?: string;
+    wechat?: string;
+    threads?: string;
+    mastodon?: string;
+    goodreads?: string;
   };
 }
 
