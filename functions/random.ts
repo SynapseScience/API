bf40098e8aba864b7b7e782c74afeaa88f26dec7
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2025-01-13 00:40:55 +0000

    rewrote random string function to match length parameter

--- functions/random.ts
+++ functions/random.ts
@@ -1,5 +1,16 @@
-export function rString(length: number) {
+export function rString(length: number): string {
   
-  return Math.random().toString(36).substring(2, 2 + length)
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomCharCode = Math.floor(Math.random() * 62);
+    if (randomCharCode < 26) {
+      result += String.fromCharCode(65 + randomCharCode);
+    } else if (randomCharCode < 52) {
+      result += String.fromCharCode(97 + (randomCharCode - 26));
+    } else {
+      result += String.fromCharCode(48 + (randomCharCode - 52));
+    }
+  }
+  return result;
   
 }
