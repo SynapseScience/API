c15d9c97d6d55e09de7530011eeaae14fc5bd1ea
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2025-01-13 00:40:33 +0000

    wrapped token generation into a try-catch handler to avoid crashes

--- routes/token.ts
+++ routes/token.ts
@@ -3,57 +3,70 @@ import Application from "../models/Application";
 import bcrypt from "bcrypt";
 
 export const run = (app, CODES: { [key: string]: string }) => {
-  
   app.get("/api/token", async (req, res) => {
+    try {
 
-    let code = req.query.code;
+      let code = req.query.code;
+
+      if (!code || !CODES[code]) {
+        return res.status(401).send({
+          message: "Invalid or expired exchange code",
+        });
+      }
+
+      const username = CODES[code];
+
+      let authenticator = req.headers.authorization;
+      if (!authenticator)
+        return res.status(400).send({
+          message: "No client credentials",
+        });
+
+      let appSignature = authenticator.replace("Basic ", "");
+
+      if (!appSignature || appSignature.length == 0) {
+        return res.status(400).send({
+          message: "No client credentials",
+        });
+      }
+
+      const clientId = appSignature.split(":")[0];
+      const client = await Application.findOne({ client_id: clientId });
+
+      if (!clientId || !client) {
+        return res.status(401).send({
+          message: "Invalid client id",
+        });
+      }
+
+      const clientSecret = appSignature.split(":")[1];
+
+      if(clientSecret) {
+
+        const isMatch = await bcrypt.compare(clientSecret, client.client_secret);
+        if(!isMatch) return res.status(401).send({
+          message: "Invalid client secret",
+        });
+
+      } else return res.status(401).send({
+        message: "No client secret provided",
+      });
+
+      const token = newToken(username, code, appSignature);
+      delete CODES[code];
+
+      res.json({
+        token,
+      });
+      
+    } catch(err) {
     
-    if (!code || !CODES[code]) {
-      return res.status(401).send({
-        message: "Invalid or expired exchange code"
+      console.error(err);
+      return res.status(500).send({
+        message: "Internal server error",
       });
-    }
-
-    const username = CODES[code];
-
-    let authenticator = req.headers.authorization;
-    if (!authenticator) return res.status(400).send({
-      message: "No client credentials"
-    });
-
-    let appSignature = authenticator.replace("Basic ", "");
     
-    if (!appSignature || appSignature.length == 0) {
-      return res.status(400).send({
-        message: "No client credentials"
-      });
     }
-
-    const clientId = appSignature.split(':')[0];
-    const client = await Application.findOne({ client_id: clientId });
-
-    if(!clientId || !client) {
-      return res.status(401).send({
-        message: "Invalid client id"
-      });
-    }
-
-    const clientSecret = appSignature.split(':')[1];
-    const isMatch = await bcrypt.compare(clientSecret, client.client_secret);
-    
-    if(!clientSecret || !isMatch) {
-      return res.status(401).send({
-        message: "Invalid client secret"
-      });
-    }
-
-    const token = newToken(username, code, appSignature);
-    delete CODES[code];
-
-    res.json({
-      token
-    });
     
   });
-  
 };
