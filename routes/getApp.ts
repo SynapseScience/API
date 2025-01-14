9b9465829428e5f6b246a0ece9deda3d4f0af3b6
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2025-01-14 22:41:26 +0000

    wrote endpoint to get a specific application from client_id

---
+++ routes/getApp.ts
@@ -0,0 +1,40 @@
+import Application from "../models/Application";
+import express from "express";
+
+export const run = (app: any): void => {
+
+  app.get("/api/application", async (req: express.Request, res: express.Response) => {
+    try {
+      const client_id = req.query.client_id as string;
+
+      if (!client_id) {
+        return res.status(400).json({ 
+          message: "Username query parameter is required" 
+        });
+      }
+
+      let application = await Application.findOne({
+        client_id 
+      });
+
+      if (!application) {
+        return res.status(404).json({ 
+          message: "Application not found" 
+        });
+      }
+
+      application = await application.publicFields();
+
+      res.status(200).json(application);
+
+    } catch (err) {
+
+      console.error(err);
+      res.status(500).json({ 
+        message: "Internal server error" 
+      });
+
+    }
+  });
+
+};
