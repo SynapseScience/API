3573e50e4c60ea6ac424491511d52046b4c833c0
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2025-02-03 21:31:36 +0000

    standardized the /token endpoint by adding grant_type and b64 encoding

--- routes/token.ts
+++ routes/token.ts
@@ -1,15 +1,24 @@
 import { newToken } from "../functions/token";
+import { rString } from "../functions/random";
 import Application from "../models/Application";
 import bcrypt from "bcrypt";
 
 export const run = (app, CODES: { [key: string]: string }) => {
-  app.get("/api/token", async (req, res) => {
+  app.post("/api/token", async (req, res) => {
     try {
+      
+      let grant_type = req.query.grant_type;
+      
+      if(grant_type != "authorization_code") {
+        return res.status(400).json({
+          message: "Param `grant_type` needs to be set to `authorization_code`"
+        })
+      }
 
       let code = req.query.code;
-
+      
       if (!code || !CODES[code]) {
-        return res.status(401).send({
+        return res.status(401).json({
           message: "Invalid or expired exchange code",
         });
       }
@@ -18,37 +27,36 @@ export const run = (app, CODES: { [key: string]: string }) => {
 
       let authenticator = req.headers.authorization;
       if (!authenticator)
-        return res.status(400).send({
+        return res.status(400).json({
           message: "No client credentials",
         });
 
       let appSignature = authenticator.replace("Basic ", "");
 
       if (!appSignature || appSignature.length == 0) {
-        return res.status(400).send({
+        return res.status(400).json({
           message: "No client credentials",
         });
       }
-
-      const clientId = appSignature.split(":")[0];
+      
+      const decoded = Buffer.from(appSignature, 'base64').toString('utf-8');
+      const [clientId, clientSecret] = decoded.split(":");
+      
       const client = await Application.findOne({ client_id: clientId });
 
       if (!clientId || !client) {
-        return res.status(401).send({
+        return res.status(401).json({
           message: "Invalid client id",
         });
       }
 
-      const clientSecret = appSignature.split(":")[1];
-
       if(clientSecret) {
-
         const isMatch = await bcrypt.compare(clientSecret, client.client_secret);
-        if(!isMatch) return res.status(401).send({
+        if(!isMatch) return res.status(401).json({
           message: "Invalid client secret",
         });
 
-      } else return res.status(401).send({
+      } else return res.status(401).json({
         message: "No client secret provided",
       });
 
@@ -56,13 +64,16 @@ export const run = (app, CODES: { [key: string]: string }) => {
       delete CODES[code];
 
       res.json({
-        token,
+        access_token: token,
+        token_type: "Bearer",
+        expires_in: 3600,
+        scope: client.permissions.join(" "),
       });
       
     } catch(err) {
     
       console.error(err);
-      return res.status(500).send({
+      return res.status(500).json({
         message: "Internal server error",
       });
     
